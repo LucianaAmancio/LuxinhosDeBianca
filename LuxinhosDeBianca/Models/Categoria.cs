1f6cf5f5b0fde5b3b11a0a4286b62fba68e421dd
LucianaAmancio <luciana.da.silva.abreu.amancio@hotmail.com>
2021-11-04 23:31:08 -0300

    Criação dos controladores Produto e Categria.

---
+++ LuxinhosDeBianca/Models/Categoria.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LuxinhosDeBianca.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int CategoriaId { get; set; }
+
+        [StringLength(100)]
+        public string CategoriaNome { get; set; }
+
+        [StringLength(500)]
+        public string DescricaoDetalhada { get; set; }
+
+        [StringLength(100)]
+        public string DescricaoCurta { get; set; }
+
+        [StringLength(100)]
+        public string Preco { get; set; }
+
+        public List<Produto> Produtos { get; set; }
+
+    }
+}
---
+++ LuxinhosDeBianca/Models/Produto.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LuxinhosDeBianca.Models
+{
+    public class Produto
+    {
+        [Key]
+        public int ProdutoId { get; set; }
+
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [StringLength(100)]
+        public string ModeloEstilo { get; set; }
+
+        [StringLength(200)]
+        public string Preco { get; set; }
+
+        [StringLength(200)]
+        public string ImagemUrl { get; set; }
+
+        public bool IsProdutoPreferido { get; set; }
+
+        public bool EmEstoque { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public virtual Categoria Categoria { get; set; }
+
+
+    }
+}
